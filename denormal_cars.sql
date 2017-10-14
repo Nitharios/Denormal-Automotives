b796f195c497cc67b3996690d9ce882d789b44df
Nathan N <Nitharios@users.noreply.github.com>
2017-10-13 18:22:12 -1000

    Started code

---
+++ denormal_cars.sql
@@ -0,0 +1,15 @@
+-- Create a new postgres user named denormal_user.
+-- CREATE USER denormal_user;
+-- -- Create a new database named denormal_cars owned by denormal_user.
+-- CREATE DATABASE denormal_cars OWNER denormal_user;
+-- Run the provided scripts/denormal_data.sql script on the denormal_cars database.
+\i scripts/denormal_data.sql;
+-- Inspect the table named car_models by running \dS and look at the data using some SELECT statements.
+
+-- In denormal_cars.sql Create a query to get a list of all make_title values in the car_models table. Without any duplicate rows, this should have 71 results.
+
+-- In denormal_cars.sql Create a query to list all model_title values where the make_code is 'VOLKS'. Without any duplicate rows, this should have 27 results.
+
+-- In denormal_cars.sql Create a query to list all make_code, model_code, model_title, and year from car_models where the make_code is 'LAM', Without any duplicate rows, this should have 136 rows.
+
+-- In denormal_cars.sql Create a query to list all fields from all car_models in years between 2010 and 2015. Without any duplicate rows, this should have 7884 rows.
